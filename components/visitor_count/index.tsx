f279bf50b5f5c00a89069b3413c978d833cb2cf1
isuslov <isuslov@me.com>
2025-03-26 12:55:39 +0300

    1

--- components/visitor_count/index.tsx
+++ components/visitor_count/index.tsx
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
 import { FaUser } from 'react-icons/fa';
 
 const dev = process.env.NODE_ENV !== 'production';
-const socketURL = dev ? 'http://localhost:3000' : 'https://0.0.0.0:8080'
+const socketURL = dev ? 'http://localhost:3000' : 'http://0.0.0.0:8080'
 const VisitorCounter = () => {
   const [visitorCount, setVisitorCount] = useState(0);
 
