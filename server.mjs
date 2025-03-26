c88d628dd0e64d3473aab72d51b21df8c077df9c
isuslov <isuslov@me.com>
2025-03-26 15:55:56 +0300

    1

--- server.mjs
+++ server.mjs
@@ -8,36 +8,35 @@ const handler = app.getRequestHandler();
 let activeConnections = 0;
 
 app.prepare().then(() => {
-  const server = createServer(handler);
+    const server = createServer(handler);
 
-  const io = new Server(server, {
-    cors: {
-      origin: dev ? '*' : 'https://nades.onrender.com',
-      methods: ['GET', 'POST'],
-    },
-    transports: ['websocket', 'polling'],
-  });
-
-  io.on('connection', (socket) => {
-    console.log('New client connected:', socket.id);
-    activeConnections++;
-    broadcastActiveConnections();
-
-    socket.on('disconnect', () => {
-      console.log('Client disconnected:', socket.id);
-      activeConnections--;
-      broadcastActiveConnections();
+    const io = new Server(server, {
+        cors: {
+            origin: dev ? '*' : 'https://nades.onrender.com',
+            methods: ['GET', 'POST'],
+        },
+        transports: ['websocket', 'polling'],
     });
-  });
 
-  function broadcastActiveConnections() {
-    const message = { count: activeConnections };
-    io.emit('visitorCountUpdate', message);
-  }
+    io.on('connection', (socket) => {
+        console.log('New client connected:', socket.id);
+        activeConnections++;
+        broadcastActiveConnections();
 
-  const port = process.env.PORT || 8080;
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`Ready on port ${port}`);
-  });
+        socket.on('disconnect', () => {
+            console.log('Client disconnected:', socket.id);
+            activeConnections--;
+            broadcastActiveConnections();
+        });
+    });
+
+    function broadcastActiveConnections() {
+        const message = { count: activeConnections };
+        io.emit('visitorCountUpdate', message);
+    }
+
+    server.listen(3584, (err) => {
+        if (err) throw err;
+        console.log(`Ready on port ${3584}`);
+    });
 });
